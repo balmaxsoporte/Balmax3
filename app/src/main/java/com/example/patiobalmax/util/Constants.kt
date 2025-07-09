ffca477c2eb0f242c648e095c14968ea272ad135
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-09 07:44:21 -0400

    Create Constants.kt

---
+++ app/src/main/java/com/example/patiobalmax/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.patiobalmax.util
+
+object Constants {
+    const val PREFS_NAME = "PatioBalmaxPrefs"
+    const val DEFAULT_ADMIN_USER = "admin"
+    const val DEFAULT_ADMIN_PASS = "admin"
+}
