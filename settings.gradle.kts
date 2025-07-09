c23bb2b813cef2b62a0f1abc35363b55409e20c8
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-09 03:13:20 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,9 @@
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
         gradlePluginPortal()
-        google()
-        mavenCentral()
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "Balmax3"
 include(":app")
