e6c8c8de649c505e7f8dc171275dbca4e0f023b9
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-09 07:45:47 -0400

    Create EstadoPuesto.kt

---
+++ app/src/main/java/com/example/patiobalmax/model/EstadoPuesto.kt
@@ -0,0 +1,10 @@
+package com.example.patiobalmax.model
+
+data class EstadoPuesto(
+    val numero: Int,
+    val tipoLugar1: String,
+    val patenteLugar1: String?,
+    val tipoLugar2: String?,
+    val patenteLugar2: String?,
+    val estado: String
+)
