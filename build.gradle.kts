653c25cee51d8eddaf93770ffc39b5d5dd0fc98d
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-09 05:40:07 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // build.gradle.kts (Project-level)
 plugins {
     id("com.android.application") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
 
 repositories {
-    google() // Importante: Este es el repositorio oficial de Google
+    google()
     mavenCentral()
 }
