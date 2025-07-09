5f63e9f86907999cf92f46e91a2469cf1371fe94
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-09 07:45:06 -0400

    Create ArchivoRegistro.kt

---
+++ app/src/main/java/com/example/patiobalmax/model/ArchivoRegistro.kt
@@ -0,0 +1,11 @@
+package com.example.patiobalmax.model
+
+data class ArchivoRegistro(
+    val patio: Int,
+    val puesto: Int,
+    val detalleLugar1: String,
+    val patenteLugar1: String,
+    val detalleLugar2: String,
+    val patenteLugar2: String,
+    val nombreArrendatario: String? = null
+)
