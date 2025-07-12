aafaff9dec3a50cb5ab963a6ac33534d462e26e1
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-11 23:08:35 -0400

    Update Constants.kt

--- app/src/main/java/com/example/patiobalmax/util/Constants.kt
+++ app/src/main/java/com/example/patiobalmax/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.patiobalmax.util
 
 object Constants {
     const val PREFS_NAME = "PatioBalmaxPrefs"
-    const val DEFAULT_ADMIN_USER = "admin"
-    const val DEFAULT_ADMIN_PASS = "admin"
+    const val EXTRA_USUARIO = "usuario"
+    const val EXTRA_PERMISOS = "permisos"
 }
