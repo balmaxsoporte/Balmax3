38d3f31a6d9e2497b6a2b79453b278eea681f6ca
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-12 03:38:54 -0400

    Update App.kt

--- app/src/main/java/com/example/patiobalmax/App.kt
+++ app/src/main/java/com/example/patiobalmax/App.kt
@@ -4,5 +4,12 @@ import android.app.Application
 import com.example.patiobalmax.database.AppDatabase
 
 class App : Application() {
+
+    // Instancia única de la base de datos
     val database by lazy { AppDatabase.getDatabase(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        // Aquí puedes inicializar otros componentes globales si es necesario
+    }
 }
