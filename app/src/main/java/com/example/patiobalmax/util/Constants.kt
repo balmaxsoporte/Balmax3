df214e1d6c6ea8c01822d5f66b09e4cbfa11e326
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-12 01:48:32 -0400

    Update Constants.kt

--- app/src/main/java/com/example/patiobalmax/util/Constants.kt
+++ app/src/main/java/com/example/patiobalmax/util/Constants.kt
@@ -10,4 +10,7 @@ object Constants {
     const val EXTRA_PATENTE_LUGAR_1 = "patente_lugar1"
     const val EXTRA_LUGAR_2 = "lugar2"
     const val EXTRA_PATENTE_LUGAR_2 = "patente_lugar2"
+    const.val EXTRA_USUARIO_NOMBRE = "usuario_nombre"
+    const.val EXTRA_USUARIO_CONTRASENA = "usuario_contrasena"
+    const.val EXTRA_USUARIO_PERMISOS = "usuario_permisos"
 }
