3e60a97e6197b9e40117eaebfb9f632da1756245
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-12 03:01:02 -0400

    Update ArchivoRegistro.kt

--- app/src/main/java/com/example/patiobalmax/model/ArchivoRegistro.kt
+++ app/src/main/java/com/example/patiobalmax/model/ArchivoRegistro.kt
@@ -1,11 +1,16 @@
 package com.example.patiobalmax.model
 
+/**
+ * Representa un registro proveniente de archivos .txt
+ * Ejemplo de línea:
+ * Patio 1,Puesto 1,Auto,AA1122,Rampla,BB1122,ING
+ */
 data class ArchivoRegistro(
-    val patio: Int,
-    val puesto: Int,
-    val detalleLugar1: String,
+    val patio: String,
+    val puesto: String,
+    val lugar1: String,
     val patenteLugar1: String,
-    val detalleLugar2: String,
+    val lugar2: String,
     val patenteLugar2: String,
     val nombreArrendatario: String? = null
 )
